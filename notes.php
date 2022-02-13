f4e79c57b9eecf57e78a77635f6e02131e4b8ccb
may arden <mayard3n@gmail.com>
2022-02-13 15:59:27 -0800

    adds notes.php revision document

---
+++ notes.php
@@ -0,0 +1,5 @@
+    <div class="body text-center">
+      <a data-bs-toggle="modal" data-bs-target=".bd-example-modal-sm">
+        <img src='images/".$i.".jpg' class="img-fluid" >
+      </a>
+    </div>
