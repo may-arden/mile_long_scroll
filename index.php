47cd6ef5199a7176085bafcdba247345ebad51b4
may arden <mayard3n@gmail.com>
2022-02-13 16:44:38 -0800

    modifies modal class ids

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
  <head>
   <title>MILE LONG SCROLL</title>
   <link rel="stylesheet" href="styles.css">
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+  
  </head>
  <body>
    <div>
@@ -23,12 +27,12 @@
   for ($i = $num_imgs; $i >= 1; $i--) {
     echo 
     "<div class='body text-center'>
-      <a data-bs-toggle='modal' data-bs-target='#my-modal".$i."'>
+      <a data-bs-toggle='modal' data-bs-target='.bd-example-modal-sm".$i."'>
         <img src='images/fullsize/".$i.".jpg' class='img-fluid' >
       </a>
     </div>";
     echo     
-    "<div class='modal fade bd-example-modal-sm' id='#my-modal".$i."' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+    "<div class='modal fade bd-example-modal-sm".$i."' id='exampleModal' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
       <div class='modal-dialog modal-sm'>
         <div class='modal-content'>
           <div class='modal-header'>
@@ -45,5 +49,8 @@
     </div>";
   }
 ?>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
  </body>
 </html>
