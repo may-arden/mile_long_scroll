a80989eac091dadbc61b53e175f65bbbabd098b6
may arden <mayard3n@gmail.com>
2022-02-13 16:07:13 -0800

    modifies modal

--- notes.php
+++ notes.php
@@ -1,5 +1,27 @@
-    <div class='body text-center'>
+    <!-- <div class='body text-center'>
       <a data-bs-toggle='modal' data-bs-target='.bd-example-modal-sm'>
         <img src='images/".$i.".jpg' class='img-fluid' >
       </a>
+    </div> -->
+
+    <div class="modal fade bd-example-modal-sm" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+          <div class="modal-header">
+            <!-- <h5 class="modal-title" id="exampleModalLabel">Modal title</h5> -->
+            <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
+          </div>
+          <div class="modal-body text-center">
+
+            <!-- <img src="https://milelongscroll.net/images/19.jpg" class="img-fluid" alt="responsive image"> -->
+            <img src="19sm.jpg" class="img-fluid">
+          </div>
+          <div class="modal-footer">
+            <p>Title of Piece Goes Here </p>
+            <!-- <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">close</button> -->
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+          </div>
+        </div>
+      </div>
     </div>
