1dc5bebc6d76f9f996ee385bd707f22483bae948
may arden <mayard3n@gmail.com>
2022-02-13 16:11:12 -0800

    adds echo statement to modify for loop to include small images folder

--- index.php
+++ index.php
@@ -27,6 +27,22 @@ $dir = "images/";
         <img src='images/".$i.".jpg' class='img-fluid' >
       </a>
     </div>";
+    echo     
+    "<div class='modal fade bd-example-modal-sm' id='exampleModal' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+      <div class='modal-dialog modal-sm'>
+        <div class='modal-content'>
+          <div class='modal-header'>
+          </div>
+          <div class='modal-body text-center'>
+            <img src='images/small/".$i.".jpg' class='img-fluid'>
+          </div>
+          <div class='modal-footer'>
+            <p>Title of Piece Goes Here </p>
+            <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+          </div>
+        </div>
+      </div>
+    </div>";
   }
 ?>
  </body>
